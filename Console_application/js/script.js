32e81a226c30c24b37fc8373dd1ba9bcc5f517f6
Sergey <karakoskinsergej@gmail.com>
2024-02-27 14:35:06 +0300

    add checks

--- Console_application/js/script.js
+++ Console_application/js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -10,8 +10,27 @@ const personalMovieDB = {
 	privat: false
 };
 
-for(let i = 0; i < 2; i++) {
+if (personalMovieDB.count < 10) {
+	alert('Просмотрено мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	alert('Вы классческий зритель');
+} else if (personalMovieDB.count >= 30) {
+	alert('Вы киноман');
+} else {
+	alert('Произошла ошибка');
+}
+
+
+for (let i = 0; i < 2; i++) {
 	let quastionMovie = prompt('Один из последних просмотренных фильмов?', '');
 	let questionGrade = prompt('На сколько оцените его?', '');
-	personalMovieDB.movies[quastionMovie] = questionGrade;
+
+	if (quastionMovie === '' || quastionMovie === null || quastionMovie.length > 50 
+		|| questionGrade === '' || questionGrade === null || questionGrade.length > 50) {
+		i--;
+	} else {
+		personalMovieDB.movies[quastionMovie] = questionGrade;
+	}
 }
+
+
